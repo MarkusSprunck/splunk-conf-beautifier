7ddd46b3c5dc5726e4a8066a9bc6b14577cc0bf4
MarkusSprunck <sprunck.markus@gmail.com>
2016-02-14 16:59:05 +0100

    Add cpp unit test and refactor folder structure

---
+++ build/Debug/Cygwin-Windows/tests/tests/string_util_test.o.d
@@ -0,0 +1,7 @@
+build/Debug/Cygwin-Windows/tests/tests/string_util_test.o: \
+ tests/string_util_test.cpp tests/string_util_test.h \
+ tests/../src/string_utils.h
+
+tests/string_util_test.h:
+
+tests/../src/string_utils.h:
