ab22e002c40fa271db370da17841896dfc6ee01c
MarkusSprunck <sprunck.markus@gmail.com>
2016-02-14 17:14:39 +0100

    Clean old stuff

--- build/Debug/Cygwin-Windows/src/line_status.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/Cygwin-Windows/src/line_status.o: src/line_status.cpp \
- src/line_status.h src/std_typedef.h src/string_utils.h
-
-src/line_status.h:
-
-src/std_typedef.h:
-
-src/string_utils.h:
--- build/Release/Cygwin-Windows/src/string_utils.o.d
+++
@@ -1,4 +0,0 @@
-build/Release/Cygwin-Windows/src/string_utils.o: src/string_utils.cpp \
- src/std_typedef.h
-
-src/std_typedef.h:
